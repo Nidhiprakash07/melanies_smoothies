1c9d1d8eb8aa019a588d4cd56fc126518ac498d4
Nidhiprakash07 <116739370+Nidhiprakash07@users.noreply.github.com>
2025-04-22 13:12:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,7 @@ st.write("The name on your smoothie will be", name_on_smoothie)
 cnx = st.connection("snowflake")
 session = cnx.session()
 from snowflake.snowpark.functions import col
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 options = st.multiselect(
     "Choose upto 5 ingredients: ",
@@ -34,6 +33,8 @@ if options:
 
     for x in options:
         options_string += x + ' '
+        my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+        st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(options_string)
 
