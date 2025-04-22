993b1e47437ac80f8bb741abffbe124b3d7759b2
Nidhiprakash07 <116739370+Nidhiprakash07@users.noreply.github.com>
2025-04-22 16:08:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if options:
       #st.write('The search value for ', x,' is ', search_on, '.')
       
       st.subheader(x + ' Nutrition Information')
-      smoothiefroot_response = requests.get("https://my.fruityvise.com/api/fruit/"+search_on)
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
       
       st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
       my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'),col('FRUIT_NAME'))
